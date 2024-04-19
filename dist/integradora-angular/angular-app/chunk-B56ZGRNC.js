9e9e22aee5fec1941428a2a9b4670c7512dbb609
FERNANDO <fgolmos10@gmail.com>
2024-04-19 16:28:14 -0600

    build

---
+++ dist/integradora-angular/angular-app/chunk-B56ZGRNC.js
@@ -0,0 +1,1 @@
+var t={UrlAuth:"http://18.227.105.11:8000/api/auth",UrlUsers:"http://18.227.105.11:8000/api/v1/users",UrlPlants:"http://18.227.105.11:8000/api/v1/plants",UrlSensors:"http://18.227.105.11:8000/api/v1/sensors",UrlMongo:"http://18.227.105.11:8000/api/v1/websocket",production:!0};export{t as a};
