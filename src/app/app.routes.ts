5fe4e721d902b34d5d20a18bcd62847cb52fda35
FERNANDO <fgolmos10@gmail.com>
2024-04-20 17:39:08 -0600

    avr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,12 @@ import { activeGuard } from './core/guards/active.guard';
 
 export const routes: Routes = [
 
-    {path:'lifeplants',loadComponent:()=>import('./components/login-nav-bar/login-nav-bar.component').then(c => c.LoginNavBarComponent),canActivate:[tokenGuard,activeGuard],children:[
+    {path:'lifeplants',loadComponent:()=>import('./components/login-nav-bar/login-nav-bar.component').then(c => c.LoginNavBarComponent),canActivate:[activeGuard,tokenGuard],children:[
         {path: '', redirectTo: 'home', pathMatch: 'full'},
-        {path: 'home', loadComponent:() => import('./pages/home/home.component').then(c => c.HomeComponent), canActivate: [tokenGuard,activeGuard]},
+        {path: 'home', loadComponent:() => import('./pages/home/home.component').then(c => c.HomeComponent), canActivate: [activeGuard,tokenGuard]},
         {path: 'plants', loadComponent:() => import('./pages/plants/plants.component').then(c => c.PlantsComponent), canActivate: [tokenGuard,activeGuard]},
         {path: 'plant/:id', loadComponent:() => import('./pages/plant/plant.component').then(c => c.PlantComponent), canActivate: [tokenGuard,activeGuard]},
-        {path: 'users',loadComponent:()=>import('./pages/users/users.component').then(c => c.UsersComponent),canActivate:[tokenGuard,AdminGuard,activeGuard]},
+        {path: 'users',loadComponent:()=>import('./pages/users/users.component').then(c => c.UsersComponent),canActivate:[tokenGuard,activeGuard,AdminGuard]},  
         {path: 'about',loadComponent:()=>import('./pages/about/about.component').then(c => c.AboutComponent),canActivate:[tokenGuard,activeGuard]},
 
         
