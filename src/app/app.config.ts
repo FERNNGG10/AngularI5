14822f66db64b52e871dd923a8cdcc74381215e9
FERNANDO <fgolmos10@gmail.com>
2024-04-22 08:00:14 -0600

    aaa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { Router, provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './core/interceptors/token.interceptor';
-import { errorhandlerInterceptor } from './core/interceptors/errorhandler.interceptor';
-
+import { errorhandlerInterceptor, setRouter } from './core/interceptors/errorhandler.interceptor';
+const router = new Router();
+setRouter(router);
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes,withComponentInputBinding()),provideHttpClient(withInterceptors([tokenInterceptor,errorhandlerInterceptor]))]
