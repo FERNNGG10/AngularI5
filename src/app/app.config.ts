2852db06dfdd627d23ca633797e09a48f61aeb5f
FERNANDO <fgolmos10@gmail.com>
2024-04-22 08:11:45 -0600

    avr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { Router, provideRouter, withComponentInputBinding } from '@angular/route
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './core/interceptors/token.interceptor';
-import { errorhandlerInterceptor } from './core/interceptors/errorhandler.interceptor';
+import { errorhandlerInterceptor, } from './core/interceptors/errorhandler.interceptor';
 
 
 export const appConfig: ApplicationConfig = {
--- src/app/core/interceptors/errorhandler.interceptor.ts
+++ src/app/core/interceptors/errorhandler.interceptor.ts
@@ -14,23 +14,4 @@ export const errorhandlerInterceptor: HttpInterceptorFn = (req, next) => {
   }))
 };
 
-// import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
-// import { Observable, catchError, throwError } from 'rxjs';
-// import { Router } from '@angular/router';
-// let router:Router;
-// export function setRouter(r:Router){
-//   router = r;
-// }
 
-// export const errorhandlerInterceptor: HttpInterceptorFn = (req, next) => {
-//   return next(req).pipe(catchError((error:HttpErrorResponse)=>{
-    
-
-//     console.log("Error Code: "+error+" Error Message: "+error.message);
-//     if(error.status === 401){
-//       console.log("Error Code: "+error.status+" Error Message: "+error.message);
-//       router.navigate(['/login']);
-//     }
-//     return throwError(error);
-//   }))
-// };
