f6428e0bf5eb5783e59dc213e7af2441459161e0
FERNANDO <fgolmos10@gmail.com>
2024-04-22 08:31:55 -0600

    nimodo

--- src/app/core/interceptors/errorhandler.interceptor.ts
+++ src/app/core/interceptors/errorhandler.interceptor.ts
@@ -5,11 +5,11 @@ import { catchError, throwError } from 'rxjs';
 export const errorhandlerInterceptor: HttpInterceptorFn = (req, next) => {
   return next(req).pipe(catchError((error:HttpErrorResponse)=>{
     
-    const router = new Router();
+    
     console.log("Error Code: "+error+" Error Message: "+error.message);
     if(error.status === 401){
       console.log("Error Code: "+error.status+" Error Message: "+error.message);
-      router.navigate(['/login']);
+      
     }
     return throwError(error);
   }))
