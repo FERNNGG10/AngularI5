47d36d61a4f5e4d1132f5481fc21d555bae5df4f
FERNANDO <fgolmos10@gmail.com>
2024-04-22 08:30:05 -0600

    AVR

--- src/app/core/interceptors/errorhandler.interceptor.ts
+++ src/app/core/interceptors/errorhandler.interceptor.ts
@@ -1,17 +1,21 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 
 export const errorhandlerInterceptor: HttpInterceptorFn = (req, next) => {
   return next(req).pipe(catchError((error:HttpErrorResponse)=>{
     
-
+    const router = new Router();
     console.log("Error Code: "+error+" Error Message: "+error.message);
     if(error.status === 401){
       console.log("Error Code: "+error.status+" Error Message: "+error.message);
-      
+      router.navigate(['/login']);
     }
     return throwError(error);
   }))
 };
+
+
+
 
 
